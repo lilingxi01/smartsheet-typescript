09131151bf4c3686348507429f73ed25dd6fc6e9
Lingxi Li <lilingxi01@gmail.com>
2024-02-01 17:24:24 -0500

    feat: add system column type support.

--- packages/smartsheet-typescript/src/columns.ts
+++ packages/smartsheet-typescript/src/columns.ts
@@ -1,14 +1,23 @@
 import { z } from 'zod';
 
-export const SystemColumnTypeSchema = z.enum([
-  'AUTO_NUMBER',
-  'CREATED_BY',
-  'CREATED_DATE',
-  'MODIFIED_BY',
-  'MODIFIED_DATE',
-]);
-export type SmartsheetSystemColumnType = z.infer<typeof SystemColumnTypeSchema>;
+/**
+ * System column type is in addition to the regular column type.
+ * It contains some additional system functionalities such as auto-numbering, created by, created date, modified by, and modified date.
+ * It needs to be used in conjunction with the regular column type (e.g. TEXT_NUMBER for auto-numbering, DATE / DATETIME for created date, etc.).
+ * If you are familiar with SQL, it is similar to the concept of a primary key, created_at, updated_at, auto-increment, etc.
+ */
+export enum SmartsheetSystemColumnType {
+  AUTO_NUMBER = 'AUTO_NUMBER',
+  CREATED_BY = 'CREATED_BY',
+  CREATED_DATE = 'CREATED_DATE',
+  MODIFIED_BY = 'MODIFIED_BY',
+  MODIFIED_DATE = 'MODIFIED_DATE',
+}
+export const SystemColumnTypeSchema = z.nativeEnum(SmartsheetSystemColumnType);
 
+/**
+ * Column type is the main type of the column. It is required for creating a new column.
+ */
 export enum SmartsheetColumnType {
   ABSTRACT_DATETIME = 'ABSTRACT_DATETIME',
   CHECKBOX = 'CHECKBOX',
@@ -23,6 +32,13 @@ export enum SmartsheetColumnType {
   TEXT_NUMBER = 'TEXT_NUMBER',
 }
 export const ColumnTypeSchema = z.nativeEnum(SmartsheetColumnType);
+
+export const AutoNumberFormatSchema = z.object({
+  prefix: z.string().optional(),
+  suffix: z.string().optional(),
+  fill: z.string().optional().describe('The fill will be a string of 0s, e.g. "0000"'),
+});
+export type SmartsheetAutoNumberFormat = z.infer<typeof AutoNumberFormatSchema>;
 
 export const NewColumnSchema = z.object({
   title: z.string(),
--- packages/smartsheet-typescript/src/schema/schema-definitions.ts
+++ packages/smartsheet-typescript/src/schema/schema-definitions.ts
@@ -1,5 +1,5 @@
-import { SmartsheetColumnType } from '@/columns';
-import { z, ZodEnum } from 'zod';
+import { SmartsheetAutoNumberFormat, SmartsheetColumnType, SmartsheetSystemColumnType } from '@/columns';
+import { ZodEnum } from 'zod';
 import { CellFormatter } from '@/cells';
 
 export type ColumnTypesRequiringOptions = 'MULTI_PICKLIST' | 'PICKLIST';
@@ -11,9 +11,11 @@ export enum SmartsheetSymbol {
 export type SmartsheetColumnDefinition<T extends SmartsheetColumnType, Options extends [string, ...string[]]> = {
   columnName: string;
   columnType: T;
+  systemColumnType?: SmartsheetSystemColumnType;
   primary?: boolean;
   validation?: boolean;
   defaultFormat?: Partial<CellFormatter>;
+  autoNumberFormat?: SmartsheetAutoNumberFormat;
 } & (T extends ColumnTypesRequiringOptions ? {
   options: ZodEnum<Options>;
   symbol?: SmartsheetSymbol | null;
